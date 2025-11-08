378891bb23575aa2911f46b3c07535641af0e28f
thxCode <thxcode0824@gmail.com>
2025-11-08 21:28:38 +0800

    refactor: expose cmds util
    Signed-off-by: thxCode <thxcode0824@gmail.com>

--- gpustack_runner/cmds/__init__.py
+++ gpustack_runner/cmds/__init__.py
@@ -3,11 +3,17 @@ from __future__ import annotations
 from .images import (
     CopyImagesSubCommand,
     ListImagesSubCommand,
+    PlatformedImage,
     SaveImagesSubCommand,
+    append_images,
+    list_images,
 )
 
 __all__ = [
     "CopyImagesSubCommand",
     "ListImagesSubCommand",
+    "PlatformedImage",
     "SaveImagesSubCommand",
+    "append_images",
+    "list_images",
 ]
